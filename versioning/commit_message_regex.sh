cfb1ff2a9a34ee21771251dab88ec89684608283
yohai korem <yohai.korem@wideops.com>
2024-12-25 15:15:03 +0200

    feature for debugging pipeline

--- versioning/commit_message_regex.sh
+++ versioning/commit_message_regex.sh
@@ -3,11 +3,11 @@
 # Concatenate all input arguments into a single string
 commit_message="$*"
 
-# Define patterns
-minor_pattern="^Merge branch fe[a-zA-Z0-9]* into dev"
-patch_pattern1="^Merge branch bf[a-zA-Z0-9]* into dev"
-patch_pattern2="^Merge branch hotfix[a-zA-Z0-9]* into main"
-major_pattern="[Bb][Rr][Ee][Aa][Kk][Ii][Nn][Gg] [Cc][Hh][Aa][Nn][Gg][Ee]"
+# Define patterns (case-insensitive matching)
+minor_pattern="^(?i)feature[a-zA-Z0-9]*"
+patch_pattern1="^(?i)bugfix[a-zA-Z0-9]*"
+patch_pattern2="^(?i)hotfix[a-zA-Z0-9]*"
+major_pattern="(?i)[Bb][Rr][Ee][Aa][Kk][Ii][Nn][Gg] [Cc][Hh][Aa][Nn][Gg][Ee]"
 
 # Check the commit message against the patterns
 if [[ $commit_message =~ $minor_pattern ]]; then
@@ -20,7 +20,3 @@ else
     echo "Error: No matching pattern" >&2
     exit 1
 fi
-
-
-
-
