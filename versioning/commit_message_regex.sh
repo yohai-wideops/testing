59ce247986d66f9bc92085a4fd6c819fc87b58b0
yohai korem <yohai.korem@wideops.com>
2024-12-25 15:38:43 +0200

    feature pipeline fix

--- versioning/commit_message_regex.sh
+++ versioning/commit_message_regex.sh
@@ -3,18 +3,21 @@
 # Concatenate all input arguments into a single string
 commit_message="$*"
 
-# Define patterns (case-insensitive matching)
-minor_pattern="^(?i)feature[a-zA-Z0-9]*"
-patch_pattern1="^(?i)bugfix[a-zA-Z0-9]*"
-patch_pattern2="^(?i)hotfix[a-zA-Z0-9]*"
-major_pattern="(?i)[Bb][Rr][Ee][Aa][Kk][Ii][Nn][Gg] [Cc][Hh][Aa][Nn][Gg][Ee]"
+# Convert commit message to lowercase for case-insensitive matching
+commit_message_lower=$(echo "$commit_message" | tr '[:upper:]' '[:lower:]')
+
+# Define patterns (now case-sensitive since input is lowercase)
+minor_pattern="^feature.*"
+patch_pattern1="^bugfix.*"
+patch_pattern2="^hotfix.*"
+major_pattern="breaking change"
 
 # Check the commit message against the patterns
-if [[ $commit_message =~ $minor_pattern ]]; then
+if [[ $commit_message_lower =~ $minor_pattern ]]; then
     echo "minor"
-elif [[ $commit_message =~ $patch_pattern1 ]] || [[ $commit_message =~ $patch_pattern2 ]]; then
+elif [[ $commit_message_lower =~ $patch_pattern1 ]] || [[ $commit_message_lower =~ $patch_pattern2 ]]; then
     echo "patch"
-elif [[ $commit_message =~ $major_pattern ]]; then
+elif [[ $commit_message_lower =~ $major_pattern ]]; then
     echo "major"
 else
     echo "Error: No matching pattern" >&2
